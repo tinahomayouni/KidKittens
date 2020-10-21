0b52e8372fc01b22da204005d0d8cc4f19393973
Tina Homayouni <tina747@Tinas-MacBook-Air.local>
2020-10-22 03:18:54 +0330

    version 0.1 compelited

--- kitten.js
+++ kitten.js
@@ -1,10 +1,17 @@
 console.log('im the kittens')
 
-chrome.runtime.onMessage.addListener(txtReciever);
+let filenames = [
+    "destiny-wiens-oHm8scWafA0-unsplash.jpg",
+    "kevin-wiegand-l8Q2nK1La_Y-unsplash.jpg",
+    "the-lucky-neko-wxobdzVMzpQ-unsplash.jpg"
+];
+let imgs = document.getElementsByTagName('img');
 
 
-function txtReciever(message , sender , sendResponse){
-
-    console.log("message" , message.txt)
+for (imgElt of imgs){
+    let r = Math.floor(Math.random() * filenames.length);
+    let file = "kittens/" + filenames[r];
+    let url = chrome.extension.getURL(file);
+    imgElt.src =  url;
 
 }
